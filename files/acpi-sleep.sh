3f3ac66e6ce6983f6215dc448ab977bd00c92ff3
Alex Portell <amportell.13@gmail.com>
2022-08-12 08:50:17 -0400

    Update acpi-sleep.sh

--- files/acpi-sleep.sh
+++ files/acpi-sleep.sh
@@ -5,8 +5,14 @@
 # Description:  Disable system suspend ACPI wakeup for USB interfaces.
 #
 
-declare -a arr_input1=`cat /proc/acpi/wakeup | grep enabled | grep -E 'EHC|XHC'`
-for str_line1 in $arr_input1; do
-    sudo `echo $str_line1 | cut -d ' ' -f 1` > /proc/acpi/wakeup
+# set IFS #
+SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
+IFS=$'\n'      # Change IFS to newline char
+
+declare -a arr_input1=(`cat /proc/acpi/wakeup | grep enabled | grep -Ei '*EHC*|*XHC*'`)
+for str_line1 in ${arr_input1[@]}; do
+        echo `echo $str_line1 | cut -d ' ' -f1` > /proc/acpi/wakeup
 done
+
+IFS=$SAVEIFS   # Restore original IFS
 exit 0
